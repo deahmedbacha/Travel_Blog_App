36bae4d54b019ed3d6bfcbb7fae6628232947045
Djamel Eddine AHMED BACHA <deahmedbacha@gmail.com>
2023-07-13 15:42:18 +0100

    Login Page

--- app/src/main/java/com/example/travelblog/MainActivity.java
+++ app/src/main/java/com/example/travelblog/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.travelblog;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mainTextView = findViewById(R.id.mainTextView);
-        mainTextView.setText("Hello Baltouz");
     }
 }
